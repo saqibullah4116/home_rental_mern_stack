b52b12b405263ae783f738c88567fcc9b5b21350
saqibullah4116 <saqibullah4116@gmail.com>
2024-05-21 01:00:54 +0500

    fetching and showing listings

--- client/src/pages/home/components/SelectingCategory.jsx
+++ client/src/pages/home/components/SelectingCategory.jsx
@@ -1,11 +1,23 @@
-import { Box, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Box,
+  Grid,
+  Typography,
+  Skeleton,
+  Card,
+  CardContent,
+  CardMedia,
+} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
 import { categories } from "../../../data";
-import { useDispatch } from "react-redux";
+import { setListings } from "../../../redux/state";
 
 const SelectingCategory = () => {
   const [category, setCategory] = useState("All");
-
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const listings = useSelector((state) => state.listings);
   const dispatch = useDispatch();
 
   const selectCategory = (item) => {
@@ -14,29 +26,38 @@ const SelectingCategory = () => {
 
   const fetchSelectedCategory = async () => {
     try {
+      setLoading(true);
       const response = await fetch(
-        `http://localhost:4000/listing?category=${category}`,
+        category !== "All"
+          ? `http://localhost:4000/listing?category=${category}`
+          : "http://localhost:4000/listing",
         {
           method: "GET",
           headers: {
-            "content-Type": "application/json",
+            "Content-Type": "application/json",
           },
         }
       );
       const fetchedCategory = await response.json();
-      console.log("**************************************************");
-      console.log(fetchedCategory);
+
       if (response.status >= 400) {
-        // setError(loggedIn.message);
+        setError(fetchedCategory.message);
+        setLoading(false);
       }
 
       if (response.ok) {
-        dispatch();
+        setLoading(false);
+        dispatch(
+          setListings({
+            listings: fetchedCategory,
+          })
+        );
       }
     } catch (error) {
-      console.log(`log in  filed: ${error}`);
+      console.log(`Fetching category failed: ${error}`);
     }
   };
+
   useEffect(() => {
     fetchSelectedCategory();
   }, [category]);
@@ -48,6 +69,7 @@ const SelectingCategory = () => {
       paddingLeft={{ sm: 5, md: 0 }}
       paddingRight={{ sm: 5, md: 0 }}
       mt={6}
+      mb={6}
     >
       <Grid container spacing={1}>
         {categories.map((item, index) => (
@@ -95,6 +117,63 @@ const SelectingCategory = () => {
           </Grid>
         ))}
       </Grid>
+      <Box mt={5}>
+        {loading ? (
+          <Box display={"flex"} flexDirection={"row"} gap={1}>
+            {[1, 2, 3].map((index) => (
+              <Skeleton
+                key={index}
+                variant="rectangular"
+                width={210}
+                height={118}
+              />
+            ))}
+          </Box>
+        ) : listings.length < 1 ? (
+          <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+            <span>No listings available for </span>
+            <Typography variant="h5" color={"#ff69b4"} fontWeight={"bold"}>
+              {"  "+category}
+            </Typography>{" "}
+          </Box>
+        ) : (
+          <Grid container spacing={2}>
+            {listings.map((listing, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card style={{backgroundColor:"#f7f7f7"}}>
+                  <Carousel>
+                    {listing.listingPhotosPath.map((path, index) => (
+                      <CardMedia
+                        key={index}
+                        component="img"
+                        height="140"
+                        image={`http://localhost:4000/${path.replace(
+                          "public",
+                          ""
+                        )}`}
+                        alt={`Listing Image ${index + 1}`}
+                      />
+                    ))}
+                  </Carousel>
+                  <CardContent>
+                    <Typography variant="h6">{listing.title}</Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      {listing.description}
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      color="black"
+                      fontWeight={"bold"}
+                    >
+                      {listing.price} USD
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
     </Box>
   );
 };
